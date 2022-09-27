ee166ab5b039e28fff96e6c6c849d957afab70a1
pagu1003 <pagu1003@student.miun.se>
2022-09-27 17:24:21 +0200

    added two cond statements

--- rps.lisp
+++ rps.lisp
@@ -1,2 +1,12 @@
-(+ 1 1)
-(print (+ 1 1))
+;;;(screen:clear-window (screen:make-window))
+(screen:with-window (screen:clear-window screen:*window*))
+(princ "Player 1 Choose (R)ock, (P)aper or (S)cissors")
+(defvar pl1 (read))
+(princ "Player 2 Choose (R)ock, (P)aper or (S)cissors")
+(defvar pl2 (read))
+(cond ((string-equal pl1 pl2)(princ "hej\n"))
+
+        ((and(string-equal pl1 "s")(string-equal pl2 "p"))(princ "nej")))
+
+(FRESH-LINE)
+(princ "klart")		
